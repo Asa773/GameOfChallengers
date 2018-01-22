ab01923a5c0a3f0564d64f5a08d7e9c38c047230
Asa Davidson <davids26@seattleu.edu>
2018-01-22 12:48:46 -0800

    Add project files.

---
+++ GameOfChallengers/GameOfChallengers/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GameOfChallengers.Models;
+
+namespace GameOfChallengers.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
