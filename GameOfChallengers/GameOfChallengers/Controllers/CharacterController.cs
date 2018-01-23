f5b1c1a16771ecde76fc27bb2693e8144357bd9d
Asa773 <asa.davidson@yahoo.com>
2018-01-22 22:56:01 -0800

    Character code

---
+++ GameOfChallengers/GameOfChallengers/Controllers/CharacterController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfChallengers.Controllers
+{
+    class CreatureController
+    {
+        public void EquipItem(int itemLoc)
+        {
+            //equip items at the end of the round to a body location
+        }
+
+        public void DropItems()
+        {
+            //drop all items when dead
+        }
+
+        public void Attack(int id)
+        {
+            //run the attack on the selected monster
+        }
+
+        public void Move(int id, int loc)
+        {
+            bool canMove = MoveTest(id, loc);
+            //move the character if they are allowed to move there
+        }
+        private bool MoveTest(int id, int loc)
+        {
+            //test if a character can move to the selected sqare on the board
+            return true;
+        }
+
+        public int DoDamage(int id, int baseDamage)
+        {
+            //get how much damage the character will do
+            int finalDamage = GetBaseDamage(id);
+            return finalDamage;
+        }
+
+        private int GetBaseDamage(int id)
+        {
+            int baseDamage = 0;//this will be based on the character stats
+            return baseDamage;
+        }
+
+        public void TestForLevelUp(int id, int xp)
+        {
+            //if character can be leveled up call private healper
+        }
+        private void LevelUp(int id, int newLevel)
+        {
+            //level up the character
+        }
+
+        public void TakeDamage(int id, int amount)
+        {
+            //character takes damage and checks for death
+        }
+
+    }
+}
---
+++ GameOfChallengers/GameOfChallengers/Models/Creature.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfChallengers.Models
+{
+    class Creature
+    {
+        public int Id { get; set; }  //unique id for each character and monster
+        public int Type { get; set; }  //character or monster
+        public string Name { get; set; } //25 char max
+        public int Level { get; set; } //1-20
+        public int Attack { get; set; }
+        public int Damage { get; set; }
+        public int Speed { get; set; }
+        public int XP { get; set; } //0-355000
+        public int MaxHealth { get; set; }
+        public int CurrHealth { get; set; }
+        public bool Alive { get; set; } // 1 is alive, 0 is dead
+        public int Loc { get; set; }  //location on game board
+
+        //*This Item class exists but is WRONG, will be updated for the item assignment*
+        public Item Head { get; set; }
+        public Item Body { get; set; }
+        public Item Feet { get; set; }
+        public Item LHand { get; set; }
+        public Item RHand { get; set; }
+        public Item LFinger { get; set; }
+        public Item RFinger { get; set; }
+
+    }
+}
