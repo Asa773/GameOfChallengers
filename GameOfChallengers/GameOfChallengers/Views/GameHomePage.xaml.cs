7e6c5b27d5ab06befb2037369c6be681b733c081
Kanika Mishra <mishrakanika@seattleu.edu>
2018-02-16 15:49:23 -0800

    Added Opening Page
    Added SignInPage and GameHomePage
    (Opening screen)
    This is temp UI will make it fancy later

---
+++ GameOfChallengers/GameOfChallengers/Views/GameHomePage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GameOfChallengers.Views
+{
+    public partial class GameHomePage : ContentPage
+    {
+        public GameHomePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Play_Command(object sender, EventArgs e)
+        {            
+
+        }
+
+        private async void Characters_Command(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Monsters_Command(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Items_Command(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ItemsPage());
+        }
+
+        private async void Scores_Command(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ GameOfChallengers/GameOfChallengers/Views/SignInPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GameOfChallengers.Views
+{
+    public partial class SignInPage : ContentPage
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void StartGame_Command(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GameHomePage());
+
+        }
+
+
+    }
+}
