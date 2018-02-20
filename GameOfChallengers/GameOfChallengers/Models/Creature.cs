7cd66b56b3f8dee9a24f9b2aea2398fd09acd461
Asa Davidson <davids26@seattleu.edu>
2018-02-19 19:10:41 -0800

    Revert "Revert "Character CRUDi""
    This reverts commit 987b99ff5a2ac610cf82c03ebd344408e8784e84.

--- GameOfChallengers/GameOfChallengers/Models/Creature.cs
+++ GameOfChallengers/GameOfChallengers/Models/Creature.cs
@@ -19,7 +19,13 @@ namespace GameOfChallengers.Models
         public bool Alive { get; set; }// 1 is alive, 0 is dead
         public int Loc { get; set; }// location on game board
 
-        public CreatureInventory CInventory { get; set; }// inventory of items equipped to this creature
+        //public CreatureInventory CInventory { get; set; }// inventory of items equipped to this creature
+
+        public Creature()
+        {
+            Alive = true;
+            //CInventory = null;
+        }
 
         public void Update(Creature newData)
         {
@@ -40,7 +46,7 @@ namespace GameOfChallengers.Models
             CurrHealth = newData.CurrHealth;
             Alive = newData.Alive;
             Loc = newData.Loc;
-            CInventory = newData.CInventory;
+            //CInventory = newData.CInventory;
         }
     }
 }
---
+++ GameOfChallengers/GameOfChallengers/Views/Character/CharacterDetailPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using GameOfChallengers.Models;
+using GameOfChallengers.ViewModels;
+
+namespace GameOfChallengers.Views.Character
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CharacterDetailPage : ContentPage
+	{
+        private CreatureDetailViewModel _viewModel;
+        public Creature Data { get; set; }
+
+        public CharacterDetailPage(CreatureDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = viewModel;
+        }
+
+        public CharacterDetailPage()
+        {
+            InitializeComponent();
+            Data = new Creature
+            {
+                Name = "Character name",
+                Id = Guid.NewGuid().ToString(),
+                Type = 0,
+                Level = 1,
+                XP = 0,
+                MaxHealth = 10,
+                CurrHealth = 10,
+                Alive = true,
+
+            };
+
+            _viewModel = new CreatureDetailViewModel(Data);
+            BindingContext = _viewModel;
+        }
+
+
+        private async void Edit_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EditCharacterPage(_viewModel));
+        }
+
+        private async void Delete_Clicked(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new DeleteCharacterPage(_viewModel));
+
+        }
+
+        private async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
