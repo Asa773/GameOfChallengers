71386aec95f9fc5a82959da6b422ea0da2f588d5
shivani <shivani patnani@LAPTOP-CD4SVPCH>
2018-02-22 14:58:50 -0800

    more changes

---
+++ GameOfChallengers/GameOfChallengers/Views/Battle/BattleOver.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GameOfChallengers.Views.Battle
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BattleOver : ContentPage
+	{
+		public BattleOver ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void NextBattle_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BattleScreen());
+        }
+    }
+}
