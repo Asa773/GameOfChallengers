c4821ac6c1809c39fa01255712b4fe940243c03e
shivani <shivani patnani@LAPTOP-CD4SVPCH>
2018-02-25 06:05:47 -0800

    nothing

--- GameOfChallengers/GameOfChallengers/Controllers/TurnController.cs
+++ GameOfChallengers/GameOfChallengers/Controllers/TurnController.cs
@@ -18,7 +18,13 @@ namespace GameOfChallengers.Controllers
 
         public bool Attack(Creature creature1, Creature creature2)
         {
+            
             //run the attack of creature1 on creature2, return if it succeeded or not
+            //for (roll =1; roll <= 20; roll++)
+            //{
+            //    creature1 score = roll + level    
+            //}
+
             //creature1 score = roll(1-20) + level + (base attack + attack boosts)
             //creature2 score = (base defense + defense boosts) + level
             //attack is successful(true) if creature1 score > creature2 score
@@ -27,7 +33,8 @@ namespace GameOfChallengers.Controllers
 
         public int DoDamage(Creature creature, int baseDamage)
         {
-            //get how much damage the character/monster will do, use getBaseDamage(creature) and a random value
+            //getBaseDamage(creature)
+                //get how much damage the character/monster will do, use getBaseDamage(creature) and a random value
             //final damage done will be weapon damage + 1/4 level, baseDamage, + a roll(1-20)
             int finalDamage = 0;
             return finalDamage;
