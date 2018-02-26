931c4b158a71635cb2b0ff7cf9f378e419ec762b
Kanika Mishra <mishrakanika@seattleu.edu>
2018-02-26 13:58:33 -0800

    Created auto battle screen, created model for leveling up

---
+++ GameOfChallengers/GameOfChallengers/Views/Battle/AutoBattleScreen.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GameOfChallengers.Views.Battle
+{
+    public partial class AutoBattleScreen : ContentPage
+    {
+        public AutoBattleScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
