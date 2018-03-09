76640ca8d6afd7f1ad45135bd09f121a1b9f1b5d
Kanika Mishra <mishrakanika@seattleu.edu>
2018-03-09 13:08:06 -0800

    Pushing work on something assign item

--- GameOfChallengers/GameOfChallengers/Views/Items/AssignItemPage.xaml.cs
+++ GameOfChallengers/GameOfChallengers/Views/Items/AssignItemPage.xaml.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using GameOfChallengers.ViewModels;
 using Xamarin.Forms;
 
 namespace GameOfChallengers.Views.Items
 {
     public partial class AssignItemPage : ContentPage
     {
+        private TeamViewModel _viewModel;
+
         public AssignItemPage()
         {
             InitializeComponent();
+            BindingContext = _viewModel = TeamViewModel.Instance;
         }
     }
 }
